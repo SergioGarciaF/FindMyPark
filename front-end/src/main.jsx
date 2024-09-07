8a0d921ca51238f3f6cf8deff825d54898341a07
Sergio Garcia Fernandez <sergiogarciafernandez4@gmail.com>
2024-09-07 17:36:10 +0200

    dev update 07-09-2024

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,13 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { Provider } from 'react-redux'
-import { store } from './store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </StrictMode>,
 )
