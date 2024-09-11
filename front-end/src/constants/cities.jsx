c71530f56a199462ceb54397b4b1cdb8c9686fdb
Sergio Garcia Fernandez <sergiogarciafernandez4@gmail.com>
2024-09-11 11:02:47 +0200

    update Baix Llobregat parkings

--- front-end/src/constants/cities.jsx
+++ front-end/src/constants/cities.jsx
@@ -1,73 +1,78 @@
 export const cities = [
   {
-    name: "Barcelona",
-    lat: 41.399634,
-    "lng": 2.165970
+    name: 'Abrera',
+    lat: 41.521848,
+    lng: 1.904992
   },
   {
-    name: "Castelldefels",
+    name: 'Barcelona',
+    lat: 41.399634,
+    lng: 2.165970
+  },
+  {
+    name: 'Begues',
+    lat: 41.333823,
+    lng: 1.919254
+  },
+  {
+    name: 'Castelldefels',
     lat: 41.277582,
     lng: 1.978727
   },
   {
-    name: "Cornellà de Llobregat",
+    name: 'Castellví de Rosanes',
+    lat: 41.449999,
+    lng: 1.899549
+  },
+  {
+    name: 'Collbató',
+    lat: 41.566665,
+    lng: 1.814741
+  },
+  {
+    name: 'Cornellà de Llobregat',
     lat: 41.358790,
     lng: 2.073500
   },
   {
-    name: "El Prat de Llobregat",
+    name: 'El Papiol',
+    lat: 41.438662,
+    lng: 2.011139
+  },
+  {
+    name: 'El Prat de Llobregat',
     lat: 41.326319,
     lng: 2.096115
   },
   {
-    name: "Esplugues de Llobregat",
+    name: 'Esparraguera',
+    lat: 41.537742,
+    lng: 1.870034
+  },
+  {
+    name: 'Esplugues de Llobregat',
     lat: 41.374147,
     lng: 2.088954
   },
   {
-    name: "Gavà",
+    name: 'Gavà',
     lat: 41.302624,
     lng: 2.004807
   },
   {
-    name: "Granollers",
+    name: 'Girona',
+    lat: 41.974616,
+    lng: 2.818895
+  },
+  {
+    name: 'Granollers',
     lat: 41.604034,
     lng: 2.287342
   },
   {
-    name: "Hospitalet de Llobregat",
+    name: 'Hospitalet de Llobregat',
     lat: 41.363383,
     lng: 2.113969
-  },
-  {
-    name: "Molins de Rei",
-    lat: 41.412988,
-    lng: 2.018183
-  },
-  {
-    name: "Montcada i Reixach",
-    lat: 41.486944,
-    lng: 2.184539
-  },
-  {
-    name: "Rubí",
-    lat: 41.494036,
-    lng: 2.033932
-  },
-  {
-    name: "Sabadell",
-    lat: 41.542364,
-    lng: 2.110157
-  },
-  {
-    name: "Sant Boi de Llobregat",
-    lat: 41.347521,
-    lng: 2.037294
-  },
-  {
-    name: "Terrassa",
-    lat: 41.569687,
-    lng: 2.014720
   },
   {
     name: 'Hostalric',
@@ -75,8 +80,98 @@ export const cities = [
     lng: 2.628362
   },
   {
-    name: 'Girona',
-    lat: 41.974616,
-    lng: 2.818895
+    name: 'La Palma de Cervelló',
+    lat: 41.413214,
+    lng: 1.970165
+  },
+  {
+    name: 'Martorell',
+    lat: 41.479442,
+    lng: 1.917314
+  },
+  {
+    name: 'Molins de Rei',
+    lat: 41.412988,
+    lng: 2.018183
+  },
+  {
+    name: 'Montcada i Reixach',
+    lat: 41.486944,
+    lng: 2.184539
+  },
+  {
+    name: 'Olesa de Montserrat',
+    lat: 41.542431,
+    lng: 1.893320
+  },
+  {
+    name: 'Pallejà',
+    lat: 41.421968,
+    lng: 1.996892
+  },
+  {
+    name: 'Rubí',
+    lat: 41.494036,
+    lng: 2.033932
+  },
+  {
+    name: 'Sabadell',
+    lat: 41.542364,
+    lng: 2.110157
+  },
+  {
+    name: 'Sant Andreu de la Barca',
+    lat: 41.448167,
+    lng: 1.971296
+  },
+  {
+    name: 'Sant Boi de Llobregat',
+    lat: 41.347521,
+    lng: 2.037294
+  },
+  {
+    name: 'Sant Climent de Llobregat',
+    lat: 41.337186,
+    lng: 1.997162
+  },
+  {
+    name: 'Sant Feliu de Llobregat',
+    lat: 41.386213,
+    lng: 2.047617
+  },
+  {
+    name: 'Sant Joan Despí',
+    lat: 41.366848,
+    lng: 2.056797
+  },
+  {
+    name: 'Sant Vicenç dels Horts',
+    lat: 41.393542,
+    lng: 2.007875
+  },
+  {
+    name: 'Santa Coloma de Cervelló',
+    lat: 41.368240,
+    lng: 2.023013
+  },
+  {
+    name: 'Terrassa',
+    lat: 41.569687,
+    lng: 2.014720
+  },
+  {
+    name: 'Torrelles de Llobregat',
+    lat: 41.355501,
+    lng: 1.979878
+  },
+  {
+    name: 'Vallirana',
+    lat: 41.384468,
+    lng: 1.933614
+  },
+  {
+    name: 'Viladecans',
+    lat: 41.316520,
+    lng: 2.012111
   }
 ];
